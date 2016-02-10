d659d39cab837b3f5bfde22b312982c0bf148d78
mickmikea <mickmikea@gmail.com>
2016-02-09 19:48:42 -0500

    what i have so far
    haven't down  the left right scrolling.

--- 240_Group_project/main.cpp
+++ 240_Group_project/main.cpp
@@ -1,12 +1,3 @@
-
-
-
-	
-	
-	
-	
-
-
 #include <iostream>
 #include "read_write_file.h"
 #include <vector>
@@ -31,29 +22,25 @@ int main()
     curs_set(1); // mouse visible
     start_color(); // color visible
 
-    WINDOW* window = NULL;
-    window = newwin(10, 10, 0, 0);
-int number_of_printed_lines =0;
+    int number_of_printed_lines =0;
     int height, width;
     int x = 0;
-	int y = 0;
-	int xover =0;
-	int yover =0;
-	move(y, x);
+    int y = 0;
+    int start_print =0;
+    move(y, x);
+    
     height = getmaxy(stdscr);  //get the largest y value on the screen
     width = getmaxx(stdscr);   //get the largest x value on the screen
 	
-        //Number of rows, number of colums, starting y, starting x
-        window = newwin(height, width, 0, 0);  //create the window
-        int ch = ' ';
-
-	getch();
-	number_of_printed_lines  =  print_text(text, number_of_printed_lines);
-	move(y, x);
+    int ch = ' ';
+    getch();
+    
+    move(y, x);
+    
     while (1)
     {
-	
-		refresh();
+	start_print =  print_text(text, start_print);
+	refresh();
 
         height = getmaxy(stdscr);
         width = getmaxx(stdscr);
@@ -61,37 +48,38 @@ int number_of_printed_lines =0;
         ch = getch();
 
         if(ch == KEY_DOWN)
-		{
+	{
+		if ((getmaxy(stdscr)- y) > 3)
+		{ 
+			y++;
+			start_print++;
+			move(y, x);
+			wscrl(stdscr, 1);
+		}
+	}
+	if(ch == KEY_UP)
+	{
+		if (y > 0)
+		{ 
+		y--;
+		move(y, x);
+		wscrl(stdscr, -1);
+		}
 			
-			if ((getmaxy(stdscr)- y) > 3)
-			{ 
-				y++;
-				move(y, x);
-				wscrl(stdscr, 1);
-				yover++;
-			}
-		}
-		if(ch == KEY_UP)
-		{
-			if (y > 0)
-			{ 
-				y--;
-				move(y, x);
-				wscrl(stdscr, -1);
-				yover--;
-			}
-			
-		}
-		if(ch == KEY_LEFT)
-		{
-			mvwprintw(stdscr, 4, 1, "Left");
-			x = moveLeft(x, width);
-		}
-		if(ch == KEY_RIGHT)
-		{
-			mvwprintw(stdscr, 4, 1, "Right");
-			x = moveRight(x, width);
-		}
+	}
+	if(ch == KEY_LEFT)
+	{
+		
+	}
+	if(ch == KEY_RIGHT)
+	{
+		
+	}
+	if(ch == 'q')
+	{
+		//exit the loop
+		break;
+	}
     }
 	delwin(window);
 	delwin(stdscr);
@@ -106,14 +94,14 @@ int number_of_printed_lines =0;
 
 int print_text (std::vector<std::string> text, int start)
 {
-int temp = text.size() + start;
+int temp =  getmaxy(stdscr)+ start; 
 	for (int i = start; i < temp; i++)
         {
             for (int j =0; j < text[i].length(); j++)
             {
-			waddch(stdscr, text[i].at(j));
+		addch(stdscr, text[i].at(j));
             }
 		waddch(stdscr, '\n');
         }
-return temp;
+return temp; //returns the line number it stop priting at
 }
